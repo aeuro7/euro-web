fb2eaf32f30bdb859856f7774e6d2d41ae0adbec
euro <tibet.c@ku.th>
2025-02-16 17:02:30 +0700

    fixbug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
     <div className="min-h-screen w-full object-cover duration-1000 animate-fadeInUp">
       <div className="absolute w-full h-10 bg-bluemain items-center justify-center flex hover:underline z-20">
-        latest version create by euro.
+        latest version create by euro1.
       </div>
       <div className="bg-gradient-to-b from-[#040405] to-[#040405] w-full min-h-[2000px]">
         <div className="h-20 z-10"></div>
@@ -13,11 +14,11 @@ export default function Home() {
 
         {/* Position the red div below Navbar */}
         <div className="relative flex items-center justify-center w-full min-h-screen">
-          <div className="absolute top-[-660px] left-2/4 transform -translate-x-2/4 w-[1500px] h-[1000px] bg-[#314561] blur-[220px] rounded-full z-10"></div>
+          <div className="absolute top-[-500px] left-2/4 transform -translate-x-2/4 sm:w-[1200px] w-[1500px] sm:h-[800px] h-[1000px] bg-[#314561] blur-[220px] rounded-full z-10"></div>
 
           {/* Section with fade-in and slide-up animation */}
           <div className="absolute top-16 z-10 h-[500px] w-4/5 sm:w-2/4 md:w-3/4 lg:w-2/4 sm:h-auto sm:top-10 sm:translate-y-0 sm:opacity-100 flex flex-col items-center justify-start transition-all duration-700 opacity-0 translate-y-10 animate-fadeInUp px-10 sm:px-10">
-          <h1 className="text-4xl text-white font-bold text-center sm:text-3xl sm:px-4">
+            <h1 className="text-4xl text-white font-bold text-center sm:text-3xl sm:px-4">
               Hi I'am Euro
             </h1>
             <p className="mt-6 text-md text-white text-center sm:text-sm sm:px-4">
